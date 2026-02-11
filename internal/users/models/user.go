912946d647c1bba8a125407998ba604c5f1c8266
NicolasBernardes <nicolasbernardes84@gmail.com>
2026-02-11 19:07:49 -0300

    user_service added

--- internal/users/models/user.go
+++ internal/users/models/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID        int64     `json:"ID"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Name      string    `json:"name" binding:"required"`
+	Email     string    `json:"email" binding:"required,email"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
