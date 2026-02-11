fc7f690c94e59258831e7cc1b3077e6f64ba44af
NicolasBernardes <nicolasbernardes84@gmail.com>
2026-02-11 18:52:44 -0300

    controllers finished

--- internal/loans/controllers/loan_controllers.go
+++ internal/loans/controllers/loan_controllers.go
@@ -1,42 +1,108 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"library/internal/loans/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type LoanController struct {
+	loanService models.LoanService
 }
 
-func NewLoanController() *LoanController {
-	return &LoanController{}
+func NewLoanController(loanService models.LoanService) *LoanController {
+	return &LoanController{loanService}
 }
 
-func (c *LoanController) RegisterRoutes(r *gin.Engine) {
-	users := r.Group("/loans")
+func (l *LoanController) RegisterRoutes(r *gin.Engine) {
+	loans := r.Group("/loans")
 
 	{
-		users.POST("", c.CreateLoan)
-		users.GET("/:id", c.GetLoan)
-		users.GET("", c.GetAllLoans)
-		users.PUT("/:id", c.UpdateLoan)
-		users.DELETE("/:id", c.DeleteLoan)
+		loans.POST("", l.CreateLoan)
+		loans.GET("/:id", l.GetLoan)
+		loans.GET("", l.GetAllLoans)
+		loans.PUT("/:id/return", l.ReturnBook)
+	}
+
+	users := r.Group("/loans/users")
+	{
+		users.GET("/:userId/loans", l.GetUserLoans)
 	}
 }
 
-func (c *LoanController) CreateLoan(ctx *gin.Context) {
+func (l *LoanController) CreateLoan(ctx *gin.Context) {
+	var request struct {
+		BookID int64 `json:"bookID"`
+		UserID int64 `json:"userID"`
+	}
 
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+	loan, err := l.loanService.CreateLoan(request.BookID, request.UserID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, loan)
 }
 
-func (c *LoanController) GetLoan(ctx *gin.Context) {
+func (l *LoanController) GetLoan(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid loan ID"})
+		return
+	}
 
+	loan, err := l.loanService.GetLoan(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, loan)
 }
 
-func (c *LoanController) GetAllLoans(ctx *gin.Context) {
+func (l *LoanController) GetAllLoans(ctx *gin.Context) {
+	loan, err := l.loanService.GetAllLoans()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
+	ctx.JSON(http.StatusOK, loan)
 }
 
-func (c *LoanController) UpdateLoan(ctx *gin.Context) {
-
+func (l *LoanController) GetUserLoans(ctx *gin.Context) {
+	userId, err := strconv.ParseInt(ctx.Param("userId"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid loan ID"})
+		return
+	}
+	loan, err := l.loanService.GetUserLoans(userId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, loan)
 }
 
-func (c *LoanController) DeleteLoan(ctx *gin.Context) {
+func (l *LoanController) ReturnBook(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid loan ID"})
+		return
+	}
 
+	err = l.loanService.ReturnBook(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.Status(http.StatusOK)
 }
