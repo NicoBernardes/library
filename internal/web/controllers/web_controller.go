fa0c25673131176fc62a6282ca710ddd67224e30
NicolasBernardes <nicolasbernardes84@gmail.com>
2026-03-10 11:21:09 -0300

    update and delete added

--- internal/web/controllers/web_controller.go
+++ internal/web/controllers/web_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
 	bookService "library/internal/books/models"
 	loanService "library/internal/loans/models"
@@ -40,6 +41,96 @@ func (wc *WebController) RegisterRoutes(router *gin.Engine) {
 	router.GET("/users", wc.ServeUsers)
 
 	router.POST("/users", wc.CreateUser)
+	router.POST("/users/:id/edit", wc.UpdateUser)
+	router.GET("/users/:id/edit", wc.EditUserForm)
+	router.POST("/users/:id/delete", wc.DeleteUser)
+}
+
+func (wc *WebController) DeleteUser(c *gin.Context) {
+	userIDStr := c.Param("id")
+
+	userID, err := strconv.ParseInt(userIDStr, 10, 64)
+	if err != nil {
+		wc.addFlashMessage(c, "ID do usuário inválido", "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	err = wc.userService.DeleteUser(userID)
+	if err != nil {
+		wc.addFlashMessage(c, "Erro ao excluir o usuário: "+err.Error(), "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	c.Redirect(http.StatusSeeOther, "/users")
+}
+
+func (wc *WebController) UpdateUser(c *gin.Context) {
+	userIDStr := c.Param("id")
+
+	userID, err := strconv.ParseInt(userIDStr, 10, 64)
+	if err != nil {
+		wc.addFlashMessage(c, "ID do usuário inválido", "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	user, err := wc.userService.GetUser(userID)
+	if err != nil {
+		wc.addFlashMessage(c, "Erro ao buscar o usuario: "+err.Error(), "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	name := c.PostForm("name")
+	email := c.PostForm("email")
+	user.Name = name
+	user.Email = email
+
+	err = wc.userService.UpdateUser(userID, user)
+	if err != nil {
+		wc.addFlashMessage(c, "Erro ao atualizar o usuario: "+err.Error(), "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	c.Redirect(http.StatusSeeOther, "/users")
+}
+
+func (wc *WebController) EditUserForm(c *gin.Context) {
+	userIDStr := c.Param("id")
+
+	userID, err := strconv.ParseInt(userIDStr, 10, 64)
+	if err != nil {
+		wc.addFlashMessage(c, "ID do usuário inválido", "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	user, err := wc.userService.GetUser(userID)
+	if err != nil {
+		wc.addFlashMessage(c, "Erro ao buscar o usuario: "+err.Error(), "error")
+		c.Redirect(http.StatusSeeOther, "/users")
+		return
+	}
+
+	flashMessage, flashType := wc.getFlashMessage(c)
+
+	data := map[string]interface{}{
+		"Title":         "Editar Usuário",
+		"User":          user,
+		"ActiveSection": "users",
+		"FlashMessage":  flashMessage,
+		"FlashType":     flashType,
+		"IsEdit":        true,
+	}
+
+	err = wc.templates.ExecuteTemplate(c.Writer, "layout", data)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Erro ao renderizar o template: %v", err)
+		return
+	}
 }
 
 func (wc *WebController) ServeHome(c *gin.Context) {
